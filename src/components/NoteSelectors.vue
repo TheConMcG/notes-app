1d36971f136453673225684586e3313578b5939e
Connor <conmmcguire@gmail.com>
2021-11-22 19:12:10 -0600

    seperate note selectors and editors to component files

---
+++ src/components/NoteSelectors.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="note-selectors">
+    <ActiveNoteSelector />
+    <OneNoteSelector />
+    <TwoNoteSelector />
+  </div>
+</template>
+
+<script>
+  import ActiveNoteSelector from './ActiveNoteSelector.vue'
+  import OneNoteSelector from './OneNoteSelector.vue'
+  import TwoNoteSelector from './TwoNoteSelector.vue'
+  export default {
+    components: {
+      ActiveNoteSelector,
+      OneNoteSelector,
+      TwoNoteSelector
+    }
+  }
+</script>
